9badbfdf85c528643603d7c7f92c5241374b0766
Monir Ullah <mullah725@gmail.com>
2024-10-23 23:24:36 +0600

    --

--- assets/JS/ajax-testing.js
+++ assets/JS/ajax-testing.js
@@ -1,27 +1,15 @@
 jQuery(document).ready(function ($) {
   $("#submit-btn").click(function () {
     $.ajax({
-      url: variable.ajax_url, // Provided by wp_localize_script
+      url: variable.ajax_url, 
       method: "POST",
       data: {
         action: "get_post_with_ajax",
-        _ajax_nonce: variable.nonce, // Ensure nonce matches the one created in PHP
-        post_per_page: 3, // Ensure this matches the key checked in PHP
+        _ajax_nonce: variable.nonce, 
+        post_per_page: 3,
       },
       success: function (response) {
         console.log("AJAX request successful:", response);
-
-        if (Array.isArray(response)) {
-          var html = "<ul>";
-
-          response.forEach(function (item) {
-            html += "<li>" + item.post_title + "</li>";
-          });
-
-          html += "</ul>";
-
-          jQuery("#show-post").html(html); // Display posts in the #show-post div
-        }
       },
       error: function (xhr, status, error) {
         console.log("AJAX request failed:", status, error);
